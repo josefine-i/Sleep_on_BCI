1c4494542b11e06110283c92b0fa6f20322eff58
Josefine Iffelsberger <109745245+josefine-i@users.noreply.github.com>
2025-01-31 17:41:54 +0100

    Add files via upload

---
+++ 01.4.2_Kinkajou_calculating_vedba_from_raw_acc_BCI23.R
@@ -0,0 +1,201 @@
+#### Calculating VeDBA from raw acc data ####
+# for BCI '23 data #
+#### In KINKAJOUS ####
+library( stringr )
+library( data.table )
+
+## a function that uses a rolling window to calculate the dynamic component of a vector
+##win_size is in samples, so a window size of 7 would be 7 acc samples, not units of time. Has to be an odd number
+##For FFT data, data appear to be 12hz, so a windowsize of 7 is ~0.5 seconds
+
+species="Potos flavus"  # Change species to the species you will be working on assuming multiple species are in the dataframe
+
+dy_acc <- function(vect, win_size = 7){
+  #vect is a vector with the acc data for one axis for one burst
+  #win-size can be changed, but should be default 7 for our data, in our case 7 observations are ca. 7 min (10 with pads)
+  pad_size <- win_size/2 - 0.5 #make sure length out == length in. Adds a number of NAs to each side of vector 
+  # buffers the vector, creates an odd number
+  
+  padded <- unlist( c(rep(NA, pad_size), vect, rep(NA, pad_size)) ) #adds the pad to the input vector
+  acc_vec <- rep(NA, length = length( vect ) ) ##creates empy output vector 
+  
+  ## sliding window
+  for(i in 1:length(vect)){
+    win <- padded[i:(i+(2*pad_size))] ## subset the window, aka isolates win_size observations at a time from the input vector
+    m_ave <- mean( win, na.rm = T ) ## take the average over the window
+    acc_comp <- vect[ i ] - m_ave ## finds the difference between the static component (mean) and the actual value. This is the dynamic component of the acceleration at this time point
+    acc_vec[i] <- acc_comp ##Store output 
+  }
+  
+  return( unlist( acc_vec) )
+}
+
+
+## read in the complete data. This is the data downloaded from Movebank with "ACC" selected 
+complete_data <- read.csv("/Users/capuchin_monkey/Documents/Sleep_on_BCI/DATA/Sleep_on_BCI_Acc.csv") 
+# check some stuff 
+class(complete_data) #complete_data <- as.data.frame( complete_data )
+head(complete_data)
+unique(complete_data$eobs.acceleration.sampling.frequency.per.axis)
+unique(complete_data$eobs.acceleration.axes)
+unique(complete_data$individual.taxon.canonical.name)
+
+#create one tag column 
+complete_data$individual_id=paste(complete_data$individual.local.identifier, complete_data$tag.local.identifier, sep="_")
+
+## this makes the timestamp into local time by adding three hours
+complete_data$timestamp <- as.POSIXct(x= complete_data$timestamp, format=c("%Y-%m-%d %H:%M:%S"), tz='UTC' ) ## turns the timestamp into a POSIX element
+complete_data$local_timestamp=lubridate::with_tz(complete_data$timestamp, tzone = "America/Panama")
+
+##subset data but check for weird browser vs r download differences in column names 
+complete_data_trim <- try(complete_data[ , c( 'individual_id', 'local_timestamp' , 'eobs.accelerations.raw', 'eobs.acceleration.sampling.frequency.per.axis', 'individual.taxon.canonical.name' ), ]) ## keep only the necessary columns, wrapped in try function, to exclude underscores etc.
+if(class(complete_data_trim)=="try-error"){
+  complete_data_trim <- complete_data[ , c( 'individual_id', 'local_timestamp' , 'eobs.accelerations.raw', 'eobs.acceleration.sampling.frequency.per.axis', 'individual.taxon.canonical.name') ] ## keep only the necessary columns
+}
+
+
+# rename the columns to simpler names
+names( complete_data_trim ) <- c( 'tag', 'local_timestamp', 'eobs_accelerations_raw', 'sampling_freq_per_axis', 'species' )
+head( complete_data_trim )
+
+# trim the data to only the rows that contain Coati data and only those rows that have ACC data
+acc_dat <- complete_data_trim[ complete_data_trim$eobs_accelerations_raw != "" & complete_data_trim$species == species, ] 
+# check the individuals 
+unique(acc_dat$tag)
+
+#checking point
+head( acc_dat )
+length( unique( acc_dat$eobs_accelerations_raw ))
+nrow(acc_dat)
+unique(acc_dat$sampling_freq_per_axis)
+## each acc burst is unique, as we would expect. This confirms that none of the rows have NAs or otherwise missing data where the ACC burst data should be
+sum(is.na(acc_dat$sampling_freq_per_axis)) # count how many NAs are in the column => should be 0
+#acc_dat=acc_dat[complete.cases(acc_dat$sampling_freq_per_axis),] #to remove missing information NA in specific columns, if that is the case
+
+
+# remove complete_data and complete_data_trim so that they don't take up so much ram
+rm( complete_data, complete_data_trim ) #allows to delete things from the environment, that are not needed anymore
+gc() #garbage collection to free up ram 
+
+#### DATA exploration ####
+
+## What data do we have for data?
+
+plot(as.numeric(as.factor(acc_dat$tag))~acc_dat$local_timestamp,cex=0.3,pch=16,main = "Accelerometry bursts",xlab="",xaxt='n',yaxt='n',ylab="ID")
+axis(2,at=1:length(unique(acc_dat$tag)),labels=sort(unique(acc_dat$tag)),las=1,cex=0.3)
+axis.POSIXct(1,at=seq(min(acc_dat$local_timestamp),max(acc_dat$local_timestamp),by="1 month"),las=2)
+
+#make a column for night during which the burst occurs 
+# night is defined as the 24 hour period from noon to noon, with the date of the night being the date on which the night starts (i.e. the date of the first noon of the night)
+acc_dat$night <- lubridate::date( acc_dat$local_timestamp - lubridate::hours(12)) 
+acc_dat$day <- lubridate::date( acc_dat$local_timestamp) #kinkajous are nocturnal 
+
+# save the tag names
+tag_names <- as.character( unique( acc_dat$tag ) ) #include animal name and identifier
+
+# make a column for time of day
+acc_dat$time <- str_split_fixed(acc_dat$local_timestamp, " ", 2)[,2] #extracting time from the date column
+
+#### Downsampling ####
+
+### downsampling sampling interval 
+#current interval
+difftime(acc_dat$local_timestamp[1], acc_dat$local_timestamp[2]) #should be every minute 
+
+#round the times to the minute during which sampling occured 
+acc_dat$floor_time = lubridate::floor_date(acc_dat$local_timestamp, unit = "mins")
+
+acc_dat2 = data.frame() #output df 
+for (i in tag_names){
+  df = acc_dat[which(acc_dat$tag == i),]
+  try = df[!duplicated(df$floor_time),]#If there are duplicate rows, only the first row is preserved
+  
+  acc_dat2 <- rbind(acc_dat2, try) #append to df
+}
+
+#current interval
+difftime(acc_dat2$floor_time[1], acc_dat2$floor_time[2]) #should be every minute 
+
+### length of ACC burst  
+length( strsplit( acc_dat2$eobs_accelerations_raw[ 1 ], " " )[[1]] ) 
+## 25 Hz * 3 Axes(XYZ) * seconds of sampling = number of samples
+## 498 ACC samples collected per sample. This is 166 samples per axis. With a sampling frequency of 25 Hz per axis, this makes roughly 6.64 second bursts
+## need to be: 246 ACC samples collected per minute. 82 samples per axis. Sampling frequency of 12.5 Hz per axis, makes roughly 6.56 second bursts
+
+
+# split each string of ACC samples so that we can parse them into X, Y, and Z axes. Each row is a burst, split into each axis and each separate measurement per axis
+d2 <- as.data.frame( str_split( acc_dat2$eobs_accelerations_raw, " ", simplify = T ) )
+
+# make the columns class numeric instead of character strings
+for(i in 1:ncol(d2)){
+  
+  d2[,i] <- as.numeric(as.character((d2[,i])))
+  
+}
+
+# name the columns based on whether the sample belongs to the x, y, or z axis
+names(d2) <- paste(rep(c("x","y","z"),ncol(d2)/3),rep(1:(ncol(d2)/3), each = 3), sep = '')
+
+# check length of d2
+length(d2) # needs to be 246 later
+
+### downsamling from 25 to 12.5 Hz (by taking every second sample)
+d3 <- d2[ ,c(paste( rep(c("x","y","z"),(ncol(d2)/3)/2), # repeats x,y,z 
+       rep(sequence((ncol(d2)/3)/2, from = c(1, 3), by = 2), each = 3), #string of odd numbers, so every 2nd number repeated 3x
+       sep = ''))]
+
+### downsampling length of burst 
+# at 12.5 Hz, with 246 samples we only have a 6.56 burst 
+# so there are only 246/3 = 82 samples per axis 
+d3 <- d2[ ,c(paste( rep(c("x","y","z"),82), # repeats x,y,z 82 times
+                    rep(sequence(82, from = c(1, 3), by = 2), each = 3), #every 2nd number 3x repeated for 82 numbers in total 
+                    sep = ''))]
+
+# add the timestamp and tag data to this dataframe. We can add it directly from acc_dat2 because d2 is just a column from acc_dat2 that has been split into several column (i.e. the rows still match those of acc_dat2)
+d3$local_timestamp <- acc_dat2$local_timestamp
+d3$tag <- acc_dat2$tag
+
+
+#### calculating VeDBA ####
+
+# show any rows for which we don't have a complete ACC burst. This would occur, for example, if there were times of the day when the sampling frequency lower or burst duration was shorter
+d3[!complete.cases(d3),]
+
+# remove any rows for which we don't have a complete ACC burst
+inds <- complete.cases(d3)
+acc_dat2 <- acc_dat2[ inds ,]
+d3 <- d3[ inds ,]
+
+## split the parsed axes into their own dataframes
+x_d <- d3[,grepl('x',names(d3))]
+head(x_d)
+
+y_d <- d3[,grepl('y',names(d3))]
+head(y_d)
+
+z_d <- d3[,grepl('z',names(d3))]
+head(z_d)
+
+#make space
+rm(try, df, d2, inds)
+gc()
+# calculate the average vedba per burst
+# Applys the dy_acc function to all the bursts -> apply a previous function to things in the list 
+acc_dat2$ave_vedba <- apply( sqrt( apply( x_d, 1, FUN = function(x) abs( dy_acc( x ) ) )**2 + apply( y_d, 1, FUN = function(x) abs( dy_acc( x ) ) )**2 + apply( z_d, 1, FUN = function(x) abs( dy_acc( x ) ) )**2) , 2, FUN = mean )
+# all x, y, z numbers and create a average mean 
+# make a column for the logs of the average vedba
+acc_dat2$log_vedba <- log( acc_dat2$ave_vedba ) #log that mean 
+
+# create folder on computer to save vedba data in 
+write.csv(acc_dat2, "/Users/capuchin_monkey/Documents/Sleep_on_BCI/Kinkajou_sleep/DATA/Kinkajou_full_night_and_day_data_BCI23.csv", row.names = F) #saves the vedba 
+head( acc_dat2 )
+write.csv(d3, "/Users/capuchin_monkey/Documents/Sleep_on_BCI/Kinkajou_sleep/DATA/Kinkajou_downsample_ACC_BCI23.csv", row.names = F) #saves downsampled ACC
+
+acc_dat2$Year <- year(acc_dat2$local_timestamp)
+for (i in tag_names){
+  print (i)
+  print(unique(acc_dat2[which(acc_dat2$tag == i),]$Year))
+}
+
+
+
---
+++ 02.4.2_Kinkajou_Threshold_analysis.R
@@ -0,0 +1,286 @@
+####### Sleep evaluation ########
+
+library(plotrix)
+
+
+
+### prep dataframe with only subset of individuals 
+unique(sleep_per_nona$tag)
+sleep_sub_indiv = sleep_per_nona[which(sleep_per_nona$tag == "Mimi_4660" | sleep_per_nona$tag == "Norah_4655" | sleep_per_nona$tag == "Bob_4661"),]
+unique(sleep_sub_indiv$tag)
+sleep_per_nona <- read.csv( "/Users/capuchin_monkey/Documents/Caps_sleep/DATA/sleep_per_nona425_allindiv.csv" )
+
+
+
+sun_trim <- sun_dat[ sun_dat$date %in% unique( as.Date(full_dat$night) ), ]
+max(str_split_fixed( sun_trim$sunset, ' ', 2 )[ , 2 ])
+min(str_split_fixed( sun_trim$sunrise, ' ', 2 )[ , 2 ])
+min(str_split_fixed( sun_trim$night_end, ' ', 2 )[ , 2 ])
+
+
+
+library(ggplot2)
+ggplot(data = full_dat) + geom_density(aes(x = log_vedba, color = tag)) 
+ggplot(data = full_dat[which(full_dat$tag == tag_names[11] |full_dat$tag == tag_names[13] ),]) + geom_density(aes(x = log_vedba, color = tag)) 
+
+
+max(unlist(thresh)) - min(unlist(thresh))
+
+
+median(d1$log_vedba) 
+mean(d1$log_vedba) 
+
+hist(d1$log_vedba, breaks = 100)
+
+quantile(d1$log_vedba, 0.49)
+
+############# THRESHOLD ANALYSIS ############
+### SPT ###
+
+#mean
+mean(sleep_per_nona$TST_sleep_per)
+#median
+median(sleep_per_nona$TST_sleep_per)
+#sd
+sd(sleep_per_nona$TST_sleep_per)
+#histogram to see distribution
+hist(sleep_per_nona$TST_sleep_per, breaks = 100) 
+
+ggplot(data = sleep_per_nona) + geom_density(aes(x = TST_sleep_per, color = tag)) 
+
+### wake bouts ###
+
+#mean
+mean(sleep_per_nona$TST_whole_day)
+#median
+median(sleep_per_nona$TST_whole_day)
+#sd
+sd(sleep_per_nona$TST_whole_day)
+#histogram to see distribution
+hist(sleep_per_nona$TST_whole_day, breaks = 100) 
+
+
+round(mean(sleep_per_nona$TST_whole_day), digits = 3)
+
+
+length(tag_names)
+length(unique(sleep_per_nona$ID))
+nrow(sleep_per_nona)
+
+################################################
+##### Evaluation for individuals#####
+
+unique(sleep_per_nona$tag)
+
+### SPT ###
+#mean
+mean(sleep_per_nona$SPT[which(sleep_per_nona$tag == "Valoy_5766")])
+#median
+median(sleep_per_nona$SPT[which(sleep_per_nona$tag == "Valoy_5766")])
+#std 
+std.error(sleep_per_nona$SPT[which(sleep_per_nona$tag == "Valoy_5766")])
+#histogram to see distribution
+hist(sleep_per_nona$SPT[which(sleep_per_nona$tag == "Valoy_5766")], breaks = 100) 
+
+### wake bouts ###
+#mean
+mean(sleep_per_nona$wake_bouts[which(sleep_per_nona$tag == "Valoy_5766")])
+#median
+median(sleep_per_nona$wake_bouts[which(sleep_per_nona$tag == "Valoy_5766")])
+#std 
+std.error(sleep_per_nona$wake_bouts[which(sleep_per_nona$tag == "Valoy_5766")])
+#histogram to see distribution
+hist(sleep_per_nona$wake_bouts[which(sleep_per_nona$tag == "Valoy_5766")], breaks = 100) 
+
+
+#######################################
+##### EXPLORATIVE PLOTS #####
+
+library(ggplot2)
+library(ggpubr)
+library(lubridate)
+#### plot SPT vs. nights #### 
+# for the first study period 
+sleep_test <- sleep_per_nona
+sleep_test$year <- lubridate::year(sleep_test$night)
+sleep_test = sleep_test[which(sleep_test$year <= 2016),]
+
+x_range <- c( min( as.numeric(sleep_test$night)), max(as.numeric(sleep_test$night )) )
+
+ggplot(sleep_test, aes(x = as.numeric(night), y=SPT)) + geom_point()+theme_classic() + xlim(x_range) + labs(title = "2015")
+ggplot(sleep_test, aes(x = as.numeric(night), y=TST)) + geom_point()+theme_classic() + xlim(x_range) + labs(title = "2015")
+ggplot(sleep_test, aes(x = as.numeric(night), y=wake_bouts)) + geom_point()+theme_classic() + xlim(x_range) + labs(title = "2015")
+
+# for the first study period 
+sleep_test <- sleep_per_nona
+sleep_test$year <- lubridate::year(sleep_test$night)
+sleep_test = sleep_test[which(sleep_test$year >= 2017),]
+
+x_range <- c( min( as.numeric(sleep_test$night)), max(as.numeric(sleep_test$night )) )
+
+ggplot(sleep_test, aes(x = as.numeric(night), y=SPT)) + geom_point()+theme_classic() + xlim(x_range) + labs(title = "2017")
+ggplot(sleep_test, aes(x = as.numeric(night), y=TST)) + geom_point()+theme_classic() + xlim(x_range) + labs(title = "2017")
+ggplot(sleep_test, aes(x = as.numeric(night), y=wake_bouts)) + geom_point()+theme_classic() + xlim(x_range) + labs(title = "2017")
+
+###########################################
+######### plot sleep of a night ########
+
+sleep_trim <- sleep_per[ !is.na( sleep_per$SPT ), ]
+## function for plotting times from noon to noon. It will make correct 12:00 - 24:00 to be 00:00 - 12:00 and 00:00 - 12:00 to be 12:00 to 24:00
+ts_func <- function( time_vec ){
+  
+  num_time <- as.numeric( as_hms( time_vec ) )
+  
+  corr_time <- ifelse( num_time < 12*60*60, num_time + 12*60*60, num_time - 12*60*60 )
+  
+  return( corr_time )
+  
+}
+
+## function for setting transparency of a color while plotting
+transp <- function(col, alpha=.5){
+  res <- apply(col2rgb(col),2, function(c) rgb(c[1]/255, c[2]/255, c[3]/255, alpha))
+  return(res)
+}
+
+### Fig 1 ###
+library(hms)
+
+sun_trim <- sun_dat[ sun_dat$date %in% unique( full_dat$night ), ]
+
+ave_sunset <- as_hms( mean( as.numeric( as_hms( sun_trim$sunset ) ) ) )
+ave_sunset <- ts_func( ave_sunset )
+
+ave_sunrise <- as_hms( mean( as.numeric( as_hms( sun_trim$sunrise ) ) ) )
+ave_sunrise <- ts_func( ave_sunrise )
+
+ave_night_start <- as_hms( mean( as.numeric( as_hms( sun_trim$night_start ) ) ) )
+ave_night_start <- ts_func( ave_night_start )
+
+ave_night_end <- as_hms( mean( as.numeric( as_hms( sun_trim$night_end ) ) ) )
+ave_night_end <- ts_func( ave_night_end )
+
+par(mar=c(6,6,4,4))
+layout(matrix(1:3, ncol = 1), widths = 1, heights = c(2,2,2.5), respect = FALSE)
+
+
+### Fig 1A
+#one night one individual their sleep & wake bouts
+
+#create subset dataframe of one night and one individual 
+full_dat <- fread( "/Users/capuchin_monkey/Documents/Caps_sleep/DATA/full_dat425_all.csv" )
+
+sleep_one <- full_dat
+sleep_one = sleep_one[which(sleep_one$night == "2018-01-19" & sleep_one$tag == "Da Vinci_5764"),]
+
+
+par(mar = c(0, 5, 0.5, 1 ) )
+
+## plot VeDBA
+x_vals <- ts_func( sleep_one$local_time )
+
+
+plot(x=x_vals, y = sleep_one$log_vedba, type = 'l', xaxt = 'n', xlab = 'Time', ylab = '', las = 1, lwd = 0.5 )
+title( ylab = "log VeDBA", line = 2.5 )
+
+## add night shading 
+polygon( x = c( ave_sunset, ave_sunrise, ave_sunrise, ave_sunset ), y = c( -10, -10, 10, 10), col = transp('grey', .25), border = NA )
+
+polygon( x = c( ave_night_start, ave_night_end, ave_night_end, ave_night_start ), y = c( -10, -10, 10, 10), col = transp('darkgrey', .25), border = NA )
+
+
+## plot onset and waking 
+one_onset <- ts_func(sleep_per_nona[which(sleep_per_nona$night == unique( sleep_one$night) & sleep_per_nona$tag == unique( sleep_one$tag)),]$onset)
+one_waking <- ts_func(sleep_per_nona[which(sleep_per_nona$night == unique( sleep_one$night) & sleep_per_nona$tag == unique( sleep_one$tag)),]$waking)
+
+abline( v = c( one_onset, one_waking ), col = '#08519C', lty = 3, lwd = 2 )
+
+# plot sleep bouts blue
+sleep_one$colors = NA
+sleep_one$colors[which(sleep_one$sleep_bouts == 0)] = "white"
+sleep_one$colors[which(sleep_one$sleep_bouts == 1)] = "#4292C6"
+    
+for (i in 1:nrow(sleep_one)){
+    abline (v = x_vals[i], col = scales::alpha(sleep_one$colors[i], 0.1))
+}
+  
+
+
+### Fig 1B
+# average VeDBA of all and night shading 
+
+par(mar = c(0, 5, 0, 1))
+
+ave_ved <- aggregate( d1$log_vedba, by = list( d1$local_time ), FUN = mean, na.rm = T ) 
+
+names( ave_ved ) <- c( 'local_time', 'ave_VeDBA')
+
+ave_ved <- ave_ved[ order( ts_func( ave_ved$local_time ) ), ]
+
+
+se_ved <- aggregate( d1$log_vedba, by = list( d1$local_time ), FUN = std.error, na.rm = T ) 
+names( se_ved ) <- c( 'local_time', 'se_VeDBA')
+
+se_ved <- se_ved[ order( ts_func( se_ved$local_time ) ), ]
+
+se_ved$se_VeDBA[ is.na( se_ved$se_VeDBA) ] <- 0
+
+x_vals <- ts_func( ave_ved$local_time )
+
+plot( x_vals , ave_ved$ave_VeDBA, type = 'l', xaxt = 'n', xlab = 'Time', ylab = '', las = 1, lwd = 0.5  )
+
+title( ylab = "log VeDBA", line = 2.5 )
+
+
+low_se <- ave_ved$ave_VeDBA - se_ved$se_VeDBA
+high_se <- ave_ved$ave_VeDBA + se_ved$se_VeDBA
+
+
+polygon( x = c( ( x_vals ) ,rev( x_vals ), x_vals[ 1 ] ), y = c( low_se, rev( high_se ), low_se[ 1 ] ), col=transp( "#2171B5" , 0.5 ), border=NA ) # fills area between the curves
+
+polygon( x = c( ave_sunset, ave_sunrise, ave_sunrise, ave_sunset ), y = c( -10, -10, 10, 10), col = transp('grey', .25), border = NA )
+
+polygon( x = c( ave_night_start, ave_night_end, ave_night_end, ave_night_start ), y = c( -10, -10, 10, 10), col = transp('darkgrey', .25), border = NA )
+
+
+### Fig 1C
+# onset and waking density and night shading 
+
+par(mar = c(4.1, 5, 0, 1))
+
+## make a density plot of the onset time and waking time
+onset_dens <- density( ts_func( sleep_trim$onset_time ) )
+
+wake_dens <- density(  ts_func( sleep_trim$waking_time ) )
+
+waso_times <- full_dat[ !is.na( full_dat$sleep_per ) & full_dat$sleep_per == 1 & full_dat$sleep_bouts == 0, 'local_time' ]
+
+waso_dens <- density( ts_func( waso_times ) )
+
+x_range <- c( 0, 24*60*60 )
+
+y_range <- c( min( onset_dens$y, wake_dens$y ), max( onset_dens$y, wake_dens$y ) )
+
+plot( onset_dens$x, onset_dens$y, type = 'l', ylab = '', xlab = 'Time', xaxt = 'n', yaxt = "n", xlim = x_range, ylim = y_range, lty = 2, las = 1, col = c( '#08519C') )
+
+#axis( 2, at = seq( 0, 0.0004, by = 0.0002 ), las = 1 )
+
+title( ylab = "Probability Density", line = 3.9 )
+
+lines( wake_dens$x, wake_dens$y, type = 'l', ylab = 'Density', xlab = 'Waking Time', xaxt = 'n', lty = 3, col = c('#4292C6' ) )
+
+
+legend( x = 'topright', cex = 0.9, col = c( '#08519C', '#4292C6' ), lty = c( 1, 1 ), legend = c( 'Sleep onset', 'Waking time' ), bty = 'n' )
+
+polygon( x = c( ave_sunset, ave_sunrise, ave_sunrise, ave_sunset ), y = c( -10, -10, 10, 10), col = transp('grey', .25), border = NA )
+
+polygon( x = c( ave_night_start, ave_night_end, ave_night_end, ave_night_start ), y = c( -10, -10, 10, 10), col = transp('darkgrey', .25), border = NA )
+
+axis( 1, at = seq( 0, 60*24*60, 3*60*60), labels = c( as_hms( seq( 12*60*60, 60*23*60, 3*60*60) ), as_hms( seq( 0, 60*12*60, 3*60*60) ) ) ) 
+
+axis( 1, at = seq( 0, 60*24*60, 1*60*60), labels = F ) 
+
+axis( 2, at = c( 0.0, 0.0002, 0.0005 ), labels = c( '0.0', '0.0002', '0.0004' ), las = 2 )
+
+#### End of Fig
+
